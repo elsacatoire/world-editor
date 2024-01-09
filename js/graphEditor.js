744cca41ac6ae52c93a688b4b37e8959aad2619e
Elsa Catoire <elsa.catoire@gmail.com>
2024-01-09 20:28:38 +0100

    hovered points to see if selected

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -15,7 +15,6 @@ class GraphEditor {
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
             const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
             if (this.hovered) {
                 this.selected = this.hovered;
                 return
@@ -23,10 +22,17 @@ class GraphEditor {
             this.graph.addPoint(mouse);
             this.selected = mouse;
         });
+        this.canvas.addEventListener("mousemove", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+        });
     }
 
     display() {
         this.graph.draw(this.ctx)
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true });
+        }
         if (this.selected) {
             this.selected.draw(this.ctx, { outline: true });
         }
